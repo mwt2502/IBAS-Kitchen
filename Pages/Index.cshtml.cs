6b01e7445de5146f1ba78b10afee093b1ef0ed94
mwt2502 <mwo2502@live.dk>
2024-10-31 13:57:41 +0100

    Skeleton

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace IBAS_Kitchen.Pages;
-
-public class IndexModel : PageModel
+namespace IBAS_Kitchen.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
+        private readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=ibaskitchen;AccountKey=r2iqjy5ldQubtmEq2J1mt3YC1ajqxntzxp0pexMmKaxMJ2qeUPxAZkkp+vEDMCBNqzgi3XUndEQx+ASt1Sl0zQ==;EndpointSuffix=core.windows.net";
+        private readonly string tableName = "IBASKitchenWeek"; 
+        private TableClient tableClient;
+        public List<MenuItem> MenuItems { get; set; }
+
+        public IndexModel()
+        {
+            tableClient = new TableClient(connectionString, tableName);
+        }
+
+        public async Task OnGetAsync()
+        {
+            MenuItems = new List<MenuItem>();
+
+            // Fetch data from Azure Table Storage
+            await foreach (var entity in tableClient.QueryAsync<TableEntity>())
+            {
+                // Convert TableEntity to MenuItem
+                var menuItem = new MenuItem
+                {
+                    PartitionKey = entity.PartitionKey,
+                    RowKey = entity.RowKey,
+                    DishName = entity.GetString("DishName"),
+                    Price = entity.GetDouble("Price") ?? 0,
+                    Day = entity.GetString("Day")
+                };
+                MenuItems.Add(menuItem);
+            }
+        }
     }
 
-    public void OnGet()
+    public class MenuItem
     {
-
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string DishName { get; set; }
+        public double Price { get; set; }
+        public string Day { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
@@ -9,7 +10,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS val ue is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
